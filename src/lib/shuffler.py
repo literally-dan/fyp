3ab8935b6b73f6446d11a5de08ef5350147c9d24
Daniel Jones <dgj470@cs.bham.ac.uk>
2018-02-14 17:20:52 +0000

    add functionality to unshuffle

--- src/lib/shuffler.py
+++ src/lib/shuffler.py
@@ -18,20 +18,65 @@ class Datasource:
         byte = math.floor(self.bitsdone/8)
         x = ((self.data[byte]&(1<<(self.bitsdone % 8)))!=0);
         self.bitsdone += 1
-        return 1 if x else 0
+        out = 1 if x else 0
+        #print(out, end='')
+        return out
+
+        def gettox(self, x):
+            bits = math.floor(math.log2(x))
+            bitstream = ""
+            for i in range(0,bits):
+                bit = str(self.getbit())
+                bitstream += bit
+
+            current = 0
+            total = 0
+            for i in bitstream:
+                total+=math.pow(2,current)*int(i)
+                current+=1
+
+            return int(total)
+
 
 
 def main():
     data = b"this is a testing string aaaaaaaaaaaaaaaaaaaaaaaabbbbbbbbbbbbbbccccccccccccccccccdddddddddddddeeeeeeeeeefffffffgggggggghhhhhhhiiiiiiiiijjjjjjjjkkkkkkllllllmmmmmmmmmnnnnnnnooooooopppppppqqqqqqrrrrrssssssssttttttttttuuuuuuuuuvvvvvvvwwwwwwwwxxxxxxxxxyyyyyyyyyyzzzzzzzzzzzz"
     d = Datasource(data)
 
-    ls = ['hello','there','my','name','is','poop','hello','again']
-    shuffle(d,ls)
+    ls = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+    done=""
+    while d.bitsleft() > 0:
+        output = shuffle(d,ls)
+        done  += unshuffle(output)
+
+    done = done[:len(data)*8]
+    done = done[::-1]
+    banana = bytes(int(done[i : i + 8], 2) for i in range(0, len(done), 8))[::-1]
+    print(banana)
+
+
+def unshuffle(ls):
+    if(len(ls) <= 1):
+        return ""
+    output = ""
+    sortedls = sorted(ls)
+    pivot = ls.index(sortedls[0])
+    leftlist = ls[:pivot]
+    rightlist = ls[pivot+1:]
+    for x in sortedls[1:]:
+        if x in leftlist:
+            output+="1"
+        else:
+            output+="0"
+
+    return output + unshuffle(leftlist) + unshuffle(rightlist)
+
+
+
 
 def shuffle(datasource,ls):
     ls = sorted(list(set(ls))) #sort and remove duplicates
     x = shuffleR(datasource,ls)
-    print(x)
     return x
 
 def shuffleR(datasource,ls):
