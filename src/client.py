c81407898183ded7de4c06789d171a39e1b76fc9
Daniel Jones <dgj470@cs.bham.ac.uk>
2018-02-16 00:31:55 +0000

    Add capability for the client to send data to the server

--- src/client.py
+++ src/client.py
@@ -4,21 +4,55 @@ from lib.datalistener import *
 import re
 from lib.obfuscation import *
 from time import sleep
+import sys
+from lib.shuffler import *
 
 LENGTHCHARCOUNT = 4
+MTU = 2**16
 
-def run(socket):
+
+def run(socket,data_store):
     pattern = "<.*?>"
-    headers = {
-            'accept-encoding': 'gzip, deflate, sdch',
-            'accept-language': 'en-us,en;q=0.8',
-            'user-agent': 'mozilla/5.0 (macintosh; intel mac os x 10_10_1) applewebkit/537.36 (khtml, like gecko) chrome/39.0.2171.95 safari/537.36',
-            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-            'referer': 'http://www.wikipedia.org/',
-            'connection': 'keep-alive',
-            }
 
-    response = requests.get('http://localhost', headers=headers).text
+    headers  = []
+    headers += [('Host', 'www.cs.bham.ac.uk')]
+    headers += [('Connection', 'keep-alive')]
+    headers += [('Pragma','no-cache')]
+    headers += [('Cache-Control','no-cache')]
+    headers += [('Upgrade-Insecure-Requests','1')]
+    headers += [('User-Agent','Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36')]
+    headers += [('Accept','text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8')]
+    headers += [('DNT','1')]
+    headers += [('Accept-Encoding','gzip, deflate')]
+    headers += [('Accept-Language', 'en-US,en;q=0.9,en-GB;q=0.8')]
+    headers += [('Cookie','_ga=GA1.3.1924440076.1506628216')]
+    headers += [('referer','google.com')]
+    headers += [('Content-Length','0')]
+    headers += [('X-Request-ID','8a5da39b-a61f-44eb-8952-c19ad81f3817')]
+
+    if(len(data_store) == 0):
+        ds = DatasourceWrapped(b"")
+    else:
+        ds = data_store[0]
+        if(ds.bitsleft() == 0):
+            if(len(data_store) > 1):
+                data_store.remove(data_store[0])
+                ds = data_store[0]
+            else:
+                ds = DatasourceWrapped(b"")
+
+
+    print(1,ds.bitsleft())
+    print(2,len(data_store))
+    headers = shuffle(ds,headers)
+    print(3,ds.bitsleft())
+
+
+    req = requests.Session()
+
+    req.headers = listofdicttodict(headers)
+
+    response = req.get(sys.argv[1]).text
 
     resultlist = []
     result = re.finditer(pattern,response)
@@ -67,20 +101,47 @@ def main():
     sock.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
     sock.bind(('localhost',33333)) #high random port number
 
+    data_store = []
+
     sock.listen(5)
     while True:
         (client,address) = sock.accept() 
         client.settimeout(300)
-        Thread(target=recv_thread,args=(client,)).start()
+        Thread(target=recv_thread,args=(client,data_store)).start()
+        Thread(target=send_thread,args=(client,data_store)).start()
 
-def recv_thread(socket):
+def send_thread(socket_obj,data_store):
         try:
             while True:
-                ret = run(socket)
+                length = len(socket_obj.recv(MTU,socket.MSG_PEEK))
+                if(length == 0):
+                    socket_obj.close()
+
+                recv = socket_obj.recv(length)
+                data_store += [DatasourceWrapped(recv)]
+
+        except Exception as e:
+            print(1,e)
+            return
+def recv_thread(socket,data_store):
+        try:
+            while True:
+                ret = run(socket,data_store)
                 if(ret != 0):
                     sleep(1)
-        except:
+        except Exception as e:
+            print(e)
             return
+
+def listofdicttodict(ls):
+    dct = {}
+    for (key,value) in ls:
+        dct[key]=value
+
+    return dct
 
 if __name__ == "__main__":
     main()
+
+
+
